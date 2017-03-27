d1bde04425026834343432e6593a97a49a6bc577
Adrian Maseda <amaseda@gmail.com>
2017-03-27 15:37:21 -0400

    classname --> className

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ class App extends Component {
       <Router>
         <div>
           <div className="nav">
-            <div classname="nav-item"><span className="nav-logo">iStocks</span></div>
-            <div classname="nav-item"><Link to="/">Home</Link></div>
-            <div classname="nav-item"><Link to="/about">About</Link></div>
+            <div className="nav-item"><span className="nav-logo">iStocks</span></div>
+            <div className="nav-item"><Link to="/">Home</Link></div>
+            <div className="nav-item"><Link to="/about">About</Link></div>
           </div>
 
           <div className="main">
