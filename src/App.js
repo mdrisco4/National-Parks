2e1f68ebf82c4aed8860634b93eb71667ca3b0b2
Andrew Whitley <awhitley1233@gmail.com>
2017-12-10 20:02:46 -0500

    update starter code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect
-} from "react-router-dom"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div></div>
-      </Router>
+        <div>
+        </div>
     );
   }
 }
