0350a4f0a5f5693f3961bc03a11904e9d63ead5c
Mike Nabil <maikel_nabil@yahoo.com>
2018-01-17 17:01:38 -0500

    updated instructions for wdi20

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lab: React Router & APIs
+
+[Instructions Here!](https://git.generalassemb.ly/ga-wdi-exercises/react-router-lab/blob/master/README.md)
--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 import axios from 'axios'
 import Dashboard from './Dashboard'
+import Search from './Search'
 import About from './About'
 import Stock from './Stock'
 import './App.css'
@@ -41,12 +42,14 @@ class App extends Component {
         <div className='nav'>
           <div className='nav-item'><span className='nav-logo'>iStocks</span></div>
           <div className='nav-item'><Link to='/stocks'>Home</Link></div>
+          <div className='nav-item'><Link to='/search'>Search</Link></div>
           <div className='nav-item'><Link to='/about'>About</Link></div>
         </div>
 
         <div className='main'>
           <Switch>
             <Route exact path='/stocks' render={() => <Dashboard stocks={this.state.stocks} />} />
+            <Route path='/search' render={() => <Search handleTrackStock={this.handleTrackStock} />} />
             <Route path='/about' component={About} />
             <Route path='/stocks/:symbol' component={Stock} />
             <Route path='/*' render={() => <Redirect to='/stocks' />} />
