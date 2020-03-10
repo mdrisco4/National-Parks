7bf1272460adc24d055be46133ccfb72d9a99710
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-03-10 13:56:57 -0400

    rendering data.  need to pull in and layout the rest of the data and style the page

--- src/ParkPage.js
+++ src/ParkPage.js
@@ -9,44 +9,48 @@ class ParkPage extends Component {
         super(props)
         this.state = {
             data: [],
-			images: [],
-			parkName: null,
-			description: null,
-			addresses: [],
-			directions: null
+            images: [],
+            parkName: null,
+            description: null,
+            addresses: [],
+            directions: null
         }
     }
-    componentDidMount () {
+    componentDidMount() {
         // const apiKey = "VmbEdcKlJcXq26ymElH8scgVWbRSSvvsiEXkt6Qv"
         let parkCode = this.props.match.params.parkCode
-	    let parkURL = 'https://developer.nps.gov/api/v1/parks?parkCode=' + parkCode + '&fields=addresses,images&api_key=VmbEdcKlJcXq26ymElH8scgVWbRSSvvsiEXkt6Qv'
+        let parkURL = 'https://developer.nps.gov/api/v1/parks?parkCode=' + parkCode + '&fields=addresses,images&api_key=VmbEdcKlJcXq26ymElH8scgVWbRSSvvsiEXkt6Qv'
 
-            
+
         fetch(parkURL)
-        .then(res => res.json())
-        .then(res => {
-            this.setState({ data: res.data })
-	      	this.setState({ parkName: res.data[0].name })
-	      	this.setState({ description: res.data[0].description })
-	      	this.setState({ directions: res.data[0].directionsInfo })
-            console.log(res)
-        })
-       
-       
-       
-        // .catch(err => {
-        //     console.error(err)
-        // })
+            .then(res => res.json())
+            .then(res => {
+                this.setState({ data: res.data })
+                this.setState({ parkName: res.data[0].name })
+                this.setState({ description: res.data[0].description })
+                this.setState({ directions: res.data[0].directionsInfo })
+                this.state.data[0].images.map(image => {
+                    this.setState({ images: [...this.state.images, image.url] })
+                })
+                this.setState({ addresses: res.data[0].addresses[0] })
+            })
+            .catch(err => {
+                console.error(err)
+            })
+
+
+
+        // console.log(res)
     }
     render() {
         return (
             <div className="park">
-                <img src="" alt="pics"/>
-                <p>{this.props.description}</p>
+                <img src="" alt="pics" />
+                <p>{this.state.description}</p>
                 <h3>Address</h3>
-                <p>{this.props.address}</p>
+                <p>{this.state.address}</p>
                 <h3>Directions</h3>
-                <p>{this.props.directions}</p>
+                <p>{this.state.directions}</p>
             </div>
         );
     }
